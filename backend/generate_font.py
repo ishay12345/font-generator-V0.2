4d000a8d1a888f0bddb7e5facbc6b937217dc5bc
ishay12345 <ishayedry1@gmail.com>
2025-07-23 13:13:28 +0300

    Update generate_font.py

--- backend/generate_font.py
+++ backend/generate_font.py
@@ -22,9 +22,9 @@ letter_map = {
 def generate_ttf(svg_folder, output_ttf):
     print("🚀 התחלת יצירת פונט...")
     font = Font()
-    font.info.familyName = "aHebrew Handwriting"
+    font.info.familyName = "dHebrew Handwriting"
     font.info.styleName = "Regular"
-    font.info.fullName = "aHebrew Handwriting"
+    font.info.fullName = "dHebrew Handwriting"
     font.info.unitsPerEm = 1000
     font.info.ascender = 800
     font.info.descender = -200
