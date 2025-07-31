6bc1fec6e45df6029efd677f223e33c9351c8de4
ishay12345 <ishayedry1@gmail.com>
2025-07-31 16:14:12 +0300

    Update generate_font.py

--- backend/generate_font.py
+++ backend/generate_font.py
@@ -77,7 +77,7 @@ def generate_ttf(svg_folder, output_ttf):
                         transform = Identity.translate(0, 120)  # הורדת ל
                         pen = TransformPen(glyph.getPen(), transform)
                     elif name == "qof":
-                        transform = Identity.translate(0, 80)  # הורדת ק
+                        transform = Identity.translate(0, -80)  # הורדת ק
                         pen = TransformPen(glyph.getPen(), transform)
                     elif name == "kaf":
                         transform = Identity.translate(0, 140)  # הורדת ק
