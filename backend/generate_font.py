1f3cdb0b498141a8d51351b2ca6c8ea133d6a206
ishay12345 <ishayedry1@gmail.com>
2025-08-21 00:00:06 +0300

    Update generate_font.py

--- backend/generate_font.py
+++ backend/generate_font.py
@@ -103,7 +103,7 @@ def generate_ttf(svg_folder, output_ttf):
 
             glyph = font.newGlyph(name)
             glyph.unicode = unicode_val
-            glyph.width = 500
+            glyph.width = 650
 
             # ✅ טיפול מיוחד באות א
             if name == "alef":
