f88b8a0430a8a91af8bc82042103e8ed0cecfd0c
ishay12345 <ishayedry1@gmail.com>
2025-08-21 14:52:27 +0300

    Update generate_font.py

--- backend/generate_font.py
+++ backend/generate_font.py
@@ -47,7 +47,7 @@ vertical_offsets = {
 # ===== הגדרות כלליות =====
 GLOBAL_Y_SHIFT = 0
 PADDING_GENERAL = 2
-PADDING_LARGE = 20
+PADDING_LARGE = 10
 GLOBAL_SCALE = 1.0
 
 # ===== טרנספורמציות מיוחדות =====
@@ -103,15 +103,15 @@ def generate_ttf(svg_folder, output_ttf):
 
             glyph = font.newGlyph(name)
             glyph.unicode = unicode_val
-            glyph.width = 500
+            glyph.width = 400
 
             # ✅ טיפול מיוחד באות א
             if name == "alef":
                 glyph.leftMargin = 70 # דוחף אותה שמאלה
                 glyph.rightMargin = 20
             else:
-                glyph.leftMargin = 4
-                glyph.rightMargin = 4
+                glyph.leftMargin = 2
+                glyph.rightMargin = 2
 
             padding = PADDING_LARGE if name in ["finalkaf", "finalpe", "finaltsadi"] else PADDING_GENERAL
             vertical_shift = vertical_offsets.get(name, 0) + GLOBAL_Y_SHIFT
